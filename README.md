687098c0cb0ffd5ea104d23d16600a762f719664
Luis Loaiza <luislp05@gmail.com>
2016-05-04 15:49:43 -0500

    Added new files and the api lib init state

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Monkey-API
-Criptext library to call to the REST API of the Monkey Protocol
+
+Criptext node JS library to call to the REST API of the Monkey Protocol.
+
+> Request example to send push notification to a known monkey ID.
+
+```javascript
+
+var MonkeyApi = require('monkey-api.js');
+
+monkeyApi = new MonkeyApi("<API-ID>","<API-KEY>");
+
+
+var dataJSON={
+	users_to:"<MonkeyId1>,<MonkeyId2>",
+	payload:{"text": "TeXt received in the push"}
+}
+
+ monkeyApi.basicRequest("POST", "/push/new", dataJSON, function(error, objResponse){
+
+ 	if(error != null){
+            
+ 	 	console.log("DATA ERROR "+error);
+
+ 	 	return;
+     }
+
+	console.log("DATA SC "+JSON.stringify(jsonObjectRes));
+          
+          
+});
+
+```
+
+
+
+
---
+++ monkey-api.js
@@ -0,0 +1,66 @@
+
+var request = require('request');
+
+(function (){
+	'use strict';
+
+  function MonkeyApi(appKey, appSecret){
+
+  	if (appKey == null || appSecret == null) {
+      throw 'Monkey - To initialize Monkey, you must provide your App Id and App Secret';
+      return;
+    }
+
+    this.appKey = appKey;
+    this.appSecret = appSecret;
+
+    this.domainUrl = 'monkey.criptext.com';
+
+    return this;
+  }
+
+
+  	var proto = MonkeyApi.prototype;
+  	var exports = this;
+  
+
+   	proto.basicRequest = function basicRequest(methodName, endpointUrl, dataObj, callback){
+	    
+	    var requestObj={
+	        method : methodName,
+	        url : 'https://'+this.domainUrl+endpointUrl,
+	        json : true,
+	        headers : {
+	          "Authorization" : "Basic " + new Buffer(process.env.MONKEY_APP_ID+":"+process.env.MONKEY_APP_KEY).toString("base64")
+	        }
+	      }
+
+	    if(dataObj)
+	    {
+	    	requestObj.body= {data: JSON.stringify(dataObj)};
+	    }
+
+	    request(requestObj,function (error, response, body) {
+
+	        callback(error, body);
+	    });
+  	}
+
+  	// Expose the class either via AMD, CommonJS or the global object
+	  if (typeof define === 'function' && define.amd) {
+	    define(function () {
+	      return MonkeyApi;
+	    });
+	  }
+	  else if (typeof module === 'object' && module.exports){
+	    module.exports = MonkeyApi;
+	  }
+	  else {
+	    exports.MonkeyApi = MonkeyApi;
+	  }
+
+})();
+
+
+
+
